12b7d5e5b37f67677c0c77a3fca2dc843ab8e2cf
Raesangur <pascalpinguin@gmail.com>
2021-03-11 20:09:31 -0500

    printing first word

--- markov_pera1107_lacp3102.py
+++ markov_pera1107_lacp3102.py
@@ -1,49 +1,49 @@
-###
-###  Gabarit pour l'application de traitement des frequences de mots dans les oeuvres d'auteurs divers
-###  Le traitement des arguments a ete inclus:
-###     Tous les arguments requis sont presents et accessibles dans args
-###     Le traitement du mode verbose vous donne un exemple de l'utilisation des arguments
-###
-###  Frederic Mailhot, 26 fevrier 2018
-###    Revise 16 avril 2018
-###    Revise 7 janvier 2020
+#
+#  Gabarit pour l'application de traitement des fréquences de mots dans les oeuvres d'auteurs divers
+#  Le traitement des arguments à été inclus:
+#     Tous les arguments requis sont présents et accessibles dans args
+#     Le traitement du mode verbose vous donne un exemple de l'utilisation des arguments
+#
+#  Frederic Mailhot, 26 février 2018
+#    Revisé 16 avril 2018
+#    Revisé 7 janvier 2020
 
-###  Parametres utilises, leur fonction et code a generer
-###
-###  -d   Deja traite dans le gabarit:  la variable rep_auth contiendra le chemin complet vers le repertoire d'auteurs
-###       La liste d'auteurs est extraite de ce repertoire, et est comprise dans la variable authors
-###
-###  -P   Si utilise, indique au systeme d'utiliser la ponctuation.  Ce qui est considÃ©re comme un signe de ponctuation
-###       est defini dans la liste PONC
-###       Si -P EST utilise, cela indique qu'on dÃ©sire conserver la ponctuation (chaque signe est alors considere
-###       comme un mot.  Par defaut, la ponctuation devrait etre retiree
-###
-###  -m   mode d'analyse:  -m 1 indique de faire les calculs avec des unigrammes, -m 2 avec des bigrammes.
-###
-###  -a   Auteur (unique a traiter).  Utile en combinaison avec -g, -G, pour la generation d'un texte aleatoire
-###       avec les caracteristiques de l'auteur indique
-###
-###  -G   Indique qu'on veut generer un texte (voir -a ci-haut), le nombre de mots Ã  generer doit Ãªtre indique
-###
-###  -g   Indique qu'on veut generer un texte (voir -a ci-haut), le nom du fichier en sortie est indique
-###
-###  -F   Indique qu'on desire connaitre le rang d'un certain mot pour un certain auteur.  L'auteur doit etre
-###       donnÃ© avec le parametre -a, et un mot doit suivre -F:   par exemple:   -a Verne -F Cyrus
-###
-###  -v   Deja traite dans le gabarit:  mode "verbose",  va imprimer les valeurs donnÃ©es en parametre
-###
-###
-###  Le systeme doit toujours traiter l'ensemble des oeuvres de l'ensemble des auteurs.  Selon la presence et la valeur
-###  des autres parametres, le systeme produira differentes sorties:
-###
-###  avec -a, -g, -G:  generation d'un texte aleatoire avec les caracteristiques de l'auteur identifie
-###  avec -a, -F:  imprimer la frequence d'un mot d'un certain auteur.  Format de sortie:  "auteur:  mot  frequence"
-###                la frequence doit Ãªtre un nombre reel entre 0 et 1, qui represente la probabilite de ce mot
-###                pour cet auteur
-###  avec -f:  indiquer l'auteur le plus probable du texte identifie par le nom de fichier qui suit -f
-###            Format de sortie:  "nom du fichier: auteur"
-###  avec ou sans -P:  indique que les calculs doivent etre faits avec ou sans ponctuation
-###  avec -v:  mode verbose, imprimera l'ensemble des valeurs des paramÃ¨tres (fait deja partie du gabarit)
+#  Paramêtres utilisés, leur fonction et code a générer
+#
+#  -d   Deja traité dans le gabarit:  la variable rep_auth contiendra le chemin complet vers le répertoire d'auteurs
+#       La liste d'auteurs est extraite de ce repertoire, et est comprise dans la variable authors
+#
+#  -P   Si utilisé, indique au systeme d'utiliser la ponctuation. Ce qui est considére comme un signe de ponctuation
+#       est défini dans la liste PONC
+#       Si -P est utilisé, cela indique qu'on désire conserver la ponctuation (chaque signe est alors considéré
+#       comme un mot.  Par défaut, la ponctuation devrait être retirée
+#
+#  -m   mode d'analyse:  -m 1 indique de faire les calculs avec des unigrammes, -m 2 avec des bigrammes.
+#
+#  -a   Auteur (unique à traiter).  Utile en combinaison avec -g, -G, pour la génération d'un texte aléatoire
+#       avec les caractéristiques de l'auteur indiqué
+#
+#  -G   Indique qu'on veut générer un texte (voir -a ci-haut), le nombre de mots à genérer doit être indiqué
+#
+#  -g   Indique qu'on veut générer un texte (voir -a ci-haut), le nom du fichier en sortie est indiqué
+#
+#  -F   Indique qu'on désire connaître le rang d'un certain mot pour un certain auteur.  L'auteur doit etre
+#       donné avec le paramêtre -a, et un mot doit suivre -F:   par exemple:   -a Verne -F Cyrus
+#
+#  -v   Déjà traité dans le gabarit:  mode "verbose",  va imprimer les valeurs données en paramêtre
+#
+#
+#  Le système doit toujours traiter l'ensemble des oeuvres de l'ensemble des auteurs.  Selon la présence et la valeur
+#  des autres paramêtres, le système produira differentes sorties:
+#
+#  avec -a, -g, -G:  génération d'un texte aléatoire avec les caractéristiques de l'auteur identifié
+#  avec -a, -F:  imprimer la fréquence d'un mot d'un certain auteur.  Format de sortie:  "auteur:  mot  fréquence"
+#                la fréquence doit être un nombre réel entre 0 et 1, qui représente la probabilité de ce mot
+#                pour cet auteur
+#  avec -f:  indiquer l'auteur le plus probable du texte identifié par le nom de fichier qui suit -f
+#            Format de sortie:  "nom du fichier: auteur"
+#  avec ou sans -P:  indique que les calculs doivent être faits avec ou sans ponctuation
+#  avec -v:  mode verbose, imprimera l'ensemble des valeurs des paramètres (fait déjà partie du gabarit)
 
 
 import math
@@ -51,21 +51,24 @@ import argparse
 import glob
 import sys
 import os
+import re
 from pathlib import Path
 from random import randint
 from random import choice
 
-### Ajouter ici les signes de ponctuation Ã  retirer
-PONC = "\\!|\\'|\\|\\)|\\(|\\,| |\\.|; |: |\\?|\\-|_|\n"
+# Ajouter ici les signes de ponctuation à retirer
+#PONC1 = "\\!|\\'|\\)|\\(|\\,| |\\.|; |: |\\?|\\-|_|\n"
+PONC = '|'.join(['\\' + x for x in ['!', "'", ')', '(', ',', ' ', '.', ';', ':', '?', '-', '_', '«', '»', '\n', '\t']])
 
-###  Vous devriez inclure vos classes et mÃ©thodes ici, qui seront appellÃ©es Ã  partir du main
+
+#  Vous devriez inclure vos classes et méthodes ici, qui seront appellées à partir du main
 
 
-### Main: lecture des paramÃ¨tres et appel des mÃ©thodes appropriÃ©es
-###
-###       argparse permet de lire les paramÃ¨tres sur la ligne de commande
-###             Certains paramÃ¨tres sont obligatoires ("required=True")
-###             Ces paramÃ¨tres doivent Ãªtres fournis Ã  python lorsque l'application est exÃ©cutÃ©e
+# Main: lecture des paramètres et appel des méthodes appropriées
+#
+#       argparse permet de lire les paramètres sur la ligne de commande
+#             Certains paramàtres sont obligatoires ("required=True")
+#             Ces paramètres doivent êtres fournis à python lorsque l'application est exécutée
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='markov_cip1_cip2.py')
     parser.add_argument('-d', required=True, help='Repertoire contenant les sous-repertoires des auteurs')
@@ -78,11 +81,12 @@ if __name__ == "__main__":
     parser.add_argument('-g', help='Nom de base du fichier de texte a generer')
     parser.add_argument('-v', action='store_true', help='Mode verbose')
     parser.add_argument('-P', action='store_true', help='Retirer la ponctuation')
+    parser.add_argument('-M', action='store_true', help='Retirer les majuscules')
     args = parser.parse_args()
 
-    ### Lecture du rÃ©pertoire des auteurs, obtenir la liste des auteurs
-    ### Note:  args.d est obligatoire
-    ### auteurs devrait comprendre la liste des rÃ©pertoires d'auteurs, peu importe le systÃ¨me d'exploitation
+    # Lecture du répertoire des auteurs, obtenir la liste des auteurs
+    # Note:  args.d est obligatoire
+    # auteurs devrait comprendre la liste des répertoires d'auteurs, peu importe le système d'exploitation
     cwd = os.getcwd()
     if os.path.isabs(args.d):
         rep_aut = args.d
@@ -92,13 +96,13 @@ if __name__ == "__main__":
     rep_aut = os.path.normpath(rep_aut)
     authors = os.listdir(rep_aut)
 
-    ### Enlever les signes de ponctuation (ou non) - DÃ©finis dans la liste PONC
+    # Enlever les signes de ponctuation (ou non) - Définis dans la liste PONC
     if args.P:
         remove_ponc = True
     else:
         remove_ponc = False
 
-    ### Si mode verbose, reflÃ©ter les valeurs des paramÃ¨tres passÃ©s sur la ligne de commande
+    # Si mode verbose, refléter les valeurs des paramètres passés sur la ligne de commande
     if args.v:
         print("Mode verbose:")
         print("Calcul avec les auteurs du repertoire: " + args.d)
@@ -116,6 +120,9 @@ if __name__ == "__main__":
         if args.P:
             print("Retirer les signes de ponctuation suivants: {0}".format(" ".join(str(i) for i in PONC)))
 
+        if args.M:
+            print("Retirer les majuscules et considérer tous les mots comme minuscules")
+
         if args.G:
             print("Generation d'un texte de " + str(args.G) + " mots")
 
@@ -128,21 +135,56 @@ if __name__ == "__main__":
             aut = a.split("/")
             print("    " + aut[-1])
 
-    rep_books = os.path.normpath(rep_aut + "\\" + authors[1])
+# À partir d'ici, vous devriez inclure les appels à votre code
+    rep_books = os.path.normpath(rep_aut + "\\" + args.a)
     books = os.listdir(rep_books)
     print(books)
 
-    book_content = []
+    # book_content = []
+    # rep_book = os.path.normpath(rep_books + '\\' + books[1])
+    # file = open(rep_book, 'r', encoding="utf8")
+    # for line in file.readlines():
+    #     if remove_ponc:
+    #         words = [x for x in re.split(PONC, line) if x != '']
+    #     else:
+    #         words = line.split()
+    #
+    #     book_content.extend(words)
+    #
+    # print(book_content)
+
+    book_content = {}
     rep_book = os.path.normpath(rep_books + '\\' + books[1])
     file = open(rep_book, 'r', encoding="utf8")
     for line in file.readlines():
-        import re
         if remove_ponc:
-            words = [x for x in re.split(PONC, line) if x is not '']
-            book_content.extend(words)
+            words = [x.lower() if args.M else x for x in re.split(PONC, line) if x != '']
         else:
-            book_content.append(line)
+            words = line.split()
+
+        if args.m == 1:
+            for word in words:
+                if len(word) <= 2:
+                    continue
+
+                if word in book_content:
+                    book_content[word] += 1
+                else:
+                    book_content[word] = 1
+        elif args.m == 2:
+            for word1, word2 in zip(words):
+                newWord = word1 + ' ' + word2
+
+                if newWord in book_content:
+                    book_content[newWord] += 1
+                else:
+                    book_content[newWord] = 1
 
     print(book_content)
 
-### Ã€ partir d'ici, vous devriez inclure les appels Ã  votre code
+    bestword = ("BonMatin", 1)
+    for word in book_content.keys():
+        if book_content[word] > bestword[1]:
+            bestword = word, book_content[word]
+
+    print(bestword)
